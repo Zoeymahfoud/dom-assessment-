c464c283f5a64f9a312460de907279163f196903
Zoeymahfoud <98282440+Zoeymahfoud@users.noreply.github.com>
2022-04-20 11:45:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+<p>
+<ul>
+    <li>What do you call a can opener that doesn’t work?
+A can’t opener!</li>
+    <li>Did you hear about the Italian chef who died?
+He pasta-way.</li>
+<li>I like elephants. Everything else is irrelephant</li>
+</ul>
+
+
+var list = [
+    { "I like elephants. Everything else is irrelephant",
+    { "Did you hear about the Italian chef who died? He pasta-way."},
+    { "What do you call a can opener that doesn’t work? A can’t opener!" }
+];
+
+var list = [
+    { "I like elephants. Everything else is irrelephant"},
+    { "Did you hear about the Italian chef who died? He pasta-way."},
+    { "What do you call a can opener that doesn’t work? A can’t opener!" }
+];
